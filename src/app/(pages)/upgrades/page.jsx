e4b1bb564030244f5d2c90083cc9bf2e20718bf7
Samuel Rosário <willisdev5@gmail.com>
2023-07-17 20:36:45 -0300

    portifolio add upgrade page

---
+++ src/app/(pages)/upgrades/page.jsx
@@ -0,0 +1,25 @@
+import Navbar from "@/app/components/navbar/page"
+export const metadata = {
+    title: "Projects",
+    description: "descrição da page"
+}
+
+export default function Projects() {
+    return (
+        <>
+            <Navbar/>
+            <section>
+                <article>
+                    <div>
+                        <h3>Do que Se trata</h3>
+                        <p>descrição da atualização</p>
+                        <span>17/07/2023</span>
+                        
+                    </div>
+                </article>
+                
+            </section>
+            
+        </>
+    )
+}
--- src/app/components/navbar/page.jsx
+++ src/app/components/navbar/page.jsx
@@ -5,8 +5,8 @@ export default function Navbar() {
     return (
         <nav className={styled.navbar}>
             <Link className={styled.links} href='/home'>Home</Link>
-            <Link className={styled.links} href='/tecnologies'>Technologies</Link>
             <Link className={styled.links} href='/projects'>Projects</Link>
+            <Link className={styled.links} href='/tecnologies'>Technologies</Link>
             <Link className={styled.links} href='/upgrades'>Upgrades</Link>
         </nav>
     )
